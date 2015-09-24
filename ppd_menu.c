5aa80e0610c54c7a6239e4eadb6311f5a0fbc190
Sultanabranman <salter_chris@hotmail.com>
2015-09-25 09:28:22 +1000

    Added comments

--- ppd_menu.c
+++ ppd_menu.c
@@ -72,26 +72,30 @@ enum menu_input display_menu(struct menu_item* menu)
 	char * end;
 	enum menu_input input;
 	
-	
+	/** Prints menu to console **/
 	for(i = 0; i < NUM_MENU_CHOICES; i++)
 	{
+		/** If it is the first line, print text before menu item **/
 		if(i == 0)
 		{
 			printf("Main Menu:\n");
 		}
+		/** If it is the fourth line, print text before menu item **/
 		if(i == 4)
 		{
 			printf("Administrator-Only Menu:\n");
 		}
+		/** Print item number and menu item name **/
 		printf("   %d: %s\n", i+1, menu[i].name);
 	}
 	printf("Select your option (1-9):\n");
+	/** Get user input and check if it is blank **/
 	if((fgets(line, LINELEN + EXTRACHARS, stdin)) == NULL)
 	{
-		fprintf(stderr, "Error: Input was blank\n");
+		fprintf(stderr, "Error: Input was blank. Please try again\n");
 		return INVALID;
 	}
-		
+	/** check user input to see if it is over 80 characters **/	
 	if(line[strlen(line)-1] != '\n')
 	{
 		read_rest_of_line();
@@ -99,18 +103,23 @@ enum menu_input display_menu(struct menu_item* menu)
 			"Please try again\n");
 		return INVALID;
 	}
-	
+	/** remove \n from user input **/
 	line[strlen(line)-1] = 0;
+	
+	/** convert input to long **/
 	input = strtol(line, &end, 0);
 	
+	/** check if any chars where entered after int input **/
 	if(*end)
 	{
 		fprintf(stderr, "Error: Input was not a number. Please try again\n");
 		return INVALID;
 	}
 	
+	/** decrement input to match array counting **/
 	--input;
 	
+	/** check if input is within valid range **/
 	if(input < 0 || input >= INVALID)
 	{
 		fprintf(stderr, "Error: Not a valid selection\n");
